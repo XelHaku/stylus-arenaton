09b4b28c50638ff1226600d1715b0c0c04aa917e
Juan Tamez <tamez.jm@gmail.com>
2024-11-28 18:43:54 -0600

    asd

--- test_contracts/src/main.rs
+++ test_contracts/src/main.rs
@@ -1,17 +1,12 @@
 // src/main.rs
-// mod debug_mint_aton;
-// use debug_mint_aton::debug_mint_aton;
-
-mod balance_of;
-use balance_of::balance_of;
-
+mod debug_mint_aton;
+use debug_mint_aton::debug_mint_aton;
 use eyre::Result; // Import Result from eyre
 
 #[tokio::main]
 async fn main() -> Result<()> { // Use eyre::Result
     // Load environment variables
-    // debug_mint_aton().await?; 
-    balance_of().await?;// Call the function directly
+    debug_mint_aton().await?; // Call the function directly
     // swap
   
     Ok(())
