02f976c4a9f2d7e31fb32520258693780915e725
Juan Tamez <tamez.jm@gmail.com>
2024-11-29 18:14:40 -0600

    asd

--- test_contracts/src/main.rs
+++ test_contracts/src/main.rs
@@ -1,12 +1,27 @@
 // src/main.rs
-mod debug_mint_aton;
-use debug_mint_aton::debug_mint_aton;
-use eyre::Result; // Import Result from eyre
 
+// Import the eyre crate for error handling
+use eyre::Result;
+
+// Declare the modules
+mod debug_mint_aton;
+mod name;
+mod balance_of;
+
+// Import the functions from the modules
+use debug_mint_aton::debug_mint_aton;
+use name::name;
+use balance_of::balance_of;
 #[tokio::main]
-async fn main() -> Result<()> { // Use eyre::Result
-    // Load environment variables
-    debug_mint_aton().await?; // Call the function directly
+async fn main() -> Result<()> {
+    // Call the functions and propagate errors using `?`
+    // debug_mint_aton().await?;
+    name().await?;
+
+
+    let player1 = "0x7e32b54800705876d3b5cfbc7d9c226a211f7c1a";
+    balance_of(player1).await?;
+   
     // swap
   
     Ok(())
