6cbd7a897cceb6b24673fbf6880716b6cf63fbda
Juan Tamez <tamez.jm@gmail.com>
2025-01-02 19:45:28 -0600

    asd

--- stylus_arenaton_engine/src/lib.rs
+++ stylus_arenaton_engine/src/lib.rs
@@ -34,6 +34,10 @@ sol_interface! {
         function isOracle(address account) external view returns (bool);
 
     function transferFrom(address from, address to, uint256 value) external returns (bool);
+
+ function approve(address spender, uint256 value) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
 }
 }
 
